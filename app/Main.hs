4640d3e4e5d7c7af2af3b44b0ee52240558b7f6e
Alex Babkin <ababkin@gmail.com>
2018-05-30 14:07:23 -0400

    make it work

--- app/Main.hs
+++ app/Main.hs
@@ -1,50 +1,35 @@
 {-# LANGUAGE NamedFieldPuns      #-}
-{-# LANGUAGE OverloadedLists     #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell     #-}
 
 module Main where
 
-import           Control.Lens                    hiding (view, (.=))
-import           Data.Aeson
-import           Data.Default                    (def)
-import           Network.AWS.DynamoDB.DeleteItem
-import           Network.AWS.DynamoDB.GetItem
-import           Network.AWS.DynamoDB.PutItem
-import           Network.AWS.DynamoDB.Scan       hiding (scan)
-import           Protolude                       hiding (get, put, (&))
-import           Qi                              (withConfig)
-import           Qi.Config.AWS.ApiGw             (ApiVerb (Delete, Get, Post))
-import           Qi.Config.AWS.DDB               (DdbAttrDef (..),
-                                                  DdbAttrType (..))
-import           Qi.Config.AWS.Lambda            (LambdaMemorySize (..),
-                                                  lpMemorySize)
-import           Qi.Config.Identifier            (DdbTableId)
-import           Qi.Program.Config.Interface     (ConfigProgram, api,
-                                                  apiMethodLambda, apiResource,
-                                                  ddbTable)
-import           Qi.Program.Lambda.Interface     (ApiLambdaProgram,
-                                                  deleteDdbRecord, getDdbRecord,
-                                                  putDdbRecord, say,
-                                                  scanDdbRecords)
-import           Qi.Util                         (internalError, result,
-                                                  success, withSuccess)
-import           Qi.Util.ApiGw
-import           Qi.Util.DDB
-import           Types
+import           Control.Lens
+import           Control.Monad               (void)
+import           Data.Default                (def)
+import           Network.HTTP.Client
+import           Network.HTTP.Client.TLS     (tlsManagerSettings)
+import           Protolude                   hiding (state)
+import           Qi                          (withConfig)
+import           Qi.Config.AWS.CW            (CwEventsRuleProfile (ScheduledEventProfile))
+import           Qi.Program.Config.Interface (ConfigProgram, cwEventLambda)
+import           Qi.Program.Lambda.Interface (CwLambdaProgram, runServant, say)
+import           Qi.Util                     (success)
+import           Realtor.Api                 (SearchCriteria (..), search)
+import           Servant.Client
 
 
--- Used the curl commands below to test-drive the endpoints (substitute your unique api stage url first):
-{-
-export API="https://5p07mx81w8.execute-api.us-east-1.amazonaws.com/v1"
-curl -v -X POST -H "Content-Type: application/json" -d "{\"name\": \"cup\", \"shape\": \"round\", \"size\": 3}" "$API/things"
-curl -v -X GET "$API/things/cup"
-curl -v -X POST -H "Content-Type: application/json" -d "{\"name\": \"chair\", \"shape\": \"square\", \"size\": 10}" "$API/things"
-curl -v -X GET "$API/things"
-curl -v -X DELETE "$API/things/cup"
-curl -v -X GET "$API/things"
--}
+criteria :: SearchCriteria
+criteria = SearchCriteria {
+    search_criteria = "Madison_NJ"
+  , city = "Madison"
+  , state = "NJ"
+  , facets = def
+  , search_controller = "Search::RecentlySoldController"
+  , types = ["property"]
+  , page_size = 10
+  , page = 1
+  }
 
 
 main :: IO ()
@@ -52,87 +37,27 @@ main = withConfig config
   where
     config :: ConfigProgram ()
     config = do
-      thingsTable <- ddbTable "things" (DdbAttrDef "name" S) def
 
-      -- create a REST API
-      api "world" >>= \world ->
-        -- create a "things" resource
-        apiResource "things" world >>= \things -> do
-          -- create a GET method that is attached to the "scan" lambda
-          apiMethodLambda "scanThings" Get
-            things def (scan thingsTable) $
-              def
-              & lpMemorySize .~ M1024
+      let ruleProfile = ScheduledEventProfile "cron(* * * * ? *)"
 
-          apiMethodLambda "putThing" Post
-            things def (put thingsTable) $
-              def
-              & lpMemorySize .~ M1024
+      void $ cwEventLambda "myEventLambda" ruleProfile eventLambda def
 
-          -- create a "thingId" slug resource under "things"
-          apiResource "{thingId}" things >>= \thing -> do
+    eventLambda
+      :: CwLambdaProgram
+    eventLambda _ = do
+      -- emit log messages that end up in the appropriate cloudwatch group/stream
+      say "tick"
+      let contentType = "application/json"
+          accept      = "application/json, text/javascript, */*; q=0.01"
+      result <- runServant tlsManagerSettings
+                        (BaseUrl Http "realtor.com" 80 "")
+                        (search (Just contentType) (Just accept) criteria)
+      case result of
+        Left err ->
+          say $ "Error: " <> show err
+        Right res ->
+          say $ "Success: " <> show res
 
-            apiMethodLambda "getThing" Get
-              thing def (get thingsTable) def
-
-            apiMethodLambda "deleteThing" Delete
-              thing def (delete thingsTable) def
-
-      return ()
-
-    scan
-      :: DdbTableId
-      -> ApiLambdaProgram
-    scan ddbTableId _ = do
-      say "scanning records..."
-      r <- scanDdbRecords ddbTableId
-      say $ "got scan response: " <> show r
-      withSuccess (r^.srsResponseStatus) $
-        result
-          (internalError . ("Parsing error: " <>))
-          (success . (toJSON :: [Thing] -> Value))
-          $ forM (r^.srsItems) parseAttrs
-
-
-    get
-      :: DdbTableId
-      -> ApiLambdaProgram
-    get ddbTableId event =
-      withPathParam "thingId" event $ \tid -> do
-        say $ "getting record with thingId: '" <> tid <> "'..."
-        r <- getDdbRecord ddbTableId $ byNameKey tid
-
-        withSuccess (r^.girsResponseStatus) $
-          result
-            (internalError . ("Parsing error: " <>))
-            (success . (toJSON :: Thing -> Value))
-            $ parseAttrs $ r^.girsItem
-
-
-    put
-      :: DdbTableId
-      -> ApiLambdaProgram
-    put ddbTableId event =
-      withDeserializedBody event $ \(thing :: Thing) -> do
-        say $ "putting record: " <>  show thing <> "..."
-        r <- putDdbRecord ddbTableId $ toAttrs thing
-        say "...done"
-
-        withSuccess (r^.pirsResponseStatus) $
-          success "successfully put thing"
-
-
-    delete
-      :: DdbTableId
-      -> ApiLambdaProgram
-    delete ddbTableId event = do
-      withPathParam "thingId" event $ \tid -> do
-        say $ "deletting record with thingId: '" <> tid <> "'..."
-        r <- deleteDdbRecord ddbTableId $ byNameKey tid
-        withSuccess (r^.dirsResponseStatus) $
-          success "successfully deleted thing"
-
-
-
+      success "lambda had executed successfully"
 
 
---
+++ lib/Realtor/Api.hs
@@ -0,0 +1,240 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeOperators         #-}
+
+module Realtor.Api where
+
+import           Control.Monad.Fail  (fail)
+import           Data.Aeson
+import           Data.Default
+import qualified Data.HashMap.Strict as HM
+import           Data.Proxy
+import           Data.Scientific
+import           GHC.Generics
+import           Network.HTTP.Client (defaultManagerSettings, newManager)
+import           Protolude
+import           Servant.API
+import           Servant.Client
+
+{-
+curl 'https://www.realtor.com/search_result.json' \
+  -H 'content-type: application/json' \
+  -H 'accept: application/json, text/javascript, */*; q=0.01' \
+  --data-binary '{"search_criteria":"Madison_NJ","city":"Madison","county":"Morris","discovery_mode":false,"state":"NH","postal":null,"sort":null,"position":null,"facets":{"beds_min":"3","beds_max":"","baths_min":null,"baths_max":null,"price_min":null,"price_max":null,"prop_type":"","sqft_min":null,"sqft_max":null,"acre_min":null,"acre_max":null,"lot_unit":null,"age_max":null,"age_min":null,"radius":null,"pets":null,"days_on_market":null,"open_house":null,"show_listings":null,"pending":null,"foreclosure":null,"new_construction":null,"multi_search":{},"include_pending_contingency":false,"features_hash":[],"prop_types":[""]},"search_controller":"Search::RecentlySoldController","neighborhood":null,"street":null,"searchType":"city","school":null,"types":["property"],"searchFacetsToDTM":["beds_min"],"searchFeaturesToDTM":[],"page_size":80,"viewport_height":216,"pin_height":25}' \
+  --compressed | jq .
+-}
+
+
+
+
+data Facets = Facets {
+    beds_min         :: Maybe Text
+  , beds_max         :: Maybe Text
+  , baths_min        :: Maybe Text
+  , baths_max        :: Maybe Text
+  , price_min        :: Maybe Text
+  , price_max        :: Maybe Text
+  , acre_min         :: Maybe Text
+  , acre_max         :: Maybe Text
+  , foreclosure      :: Maybe Bool
+  , days_on_market   :: Maybe Int
+  , new_construction :: Maybe Bool
+  }
+  deriving (Show, Generic)
+
+instance ToJSON Facets
+
+instance Default Facets where
+  def = Facets def def def def def def def def def def def
+
+data SearchCriteria = SearchCriteria {
+    search_criteria   :: Text
+  , city              :: Text
+  , state             :: Text
+  , facets            :: Facets
+  , search_controller :: Text
+  , types             :: [Text]
+  , page_size         :: Int
+  , page              :: Int
+  }
+  deriving (Show, Generic)
+
+instance ToJSON SearchCriteria
+
+
+
+{-
+  "is_saved": "false",
+  "search_id": null,
+  "seo_url": "/soldhomeprices/Nashua_NH/beds-3?pgsz=80&pos=42.763275,-71.579493,42.790491,-71.412467,13",
+  "client_polygon": null,
+  "results": {
+    "property": {
+      "type": "property",
+      "count": 78,
+      "total": 79,
+      "items": {
+        "3717832792": {
+...
+        }
+      }
+    }
+  }
+  -}
+data QueryResponse = QueryResponse {
+    total :: Int
+  , items :: [Item]
+  } deriving (Show)
+
+instance FromJSON QueryResponse where
+  parseJSON = withObject "QueryResponse" $ \ qr -> do
+    results <- qr .: "results"
+    property <- results .: "property"
+    (Object items') <- property .: "items"
+    (Number total') <- property .: "total"
+    case floatingOrInteger total' of
+      Right (total'' :: Integer) ->
+        QueryResponse
+          <$> pure (fromIntegral total'')
+          <*> (HM.elems <$> traverse parseJSON items')
+      Left (_f :: Double) ->
+        fail "encountered float in 'total' pagination field"
+
+{-
+        "3717832792": {
+          "agentId": null,
+          "ldpUrl": "/realestateandhomes-detail/14-Dow-St_Nashua_NH_03064_M37178-32792",
+          "id": "3717832792",
+          "property_id": "3717832792",
+          "source_id": "M",
+          "plan_id": 3717832792,
+          "subdivision_id": null,
+          "listing_id": 3717832792,
+          "isStatusPending": null,
+          "isSaved": false,
+          "mprId": 3717832792,
+          "officeName": null,
+          "plot": true,
+          "position": {
+            "coordinates": [
+              -71.463906,
+              42.76724
+            ],
+            "type": "Point"
+          },
+          "status": "recently_sold",
+          "type": "property",
+          "product_type": "basic",
+          "search_flags": {
+            "is_price_reduced": false,
+            "is_pending": false,
+            "is_new_listing": false,
+            "is_short_sale": null,
+            "is_showcase": false,
+            "has_tour": false,
+            "has_video": false,
+            "has_realtor_logo": false,
+            "is_cobroke": false,
+            "is_coshow_product": false,
+            "is_foreclosure": false,
+            "is_new_plan": false,
+            "price_excludes_land": null,
+            "has_deals": null,
+            "is_new": null,
+            "is_costar": false,
+            "is_address_suppressed": false,
+            "is_suppress_map_pin": false,
+            "is_suppress_map": false,
+            "has_sign_rider": null,
+            "is_aptlist": false
+          },
+          "open_house_display": null,
+          "sort": 0,
+          "isCoBroke": false,
+          "isEnhanced": false,
+          "isForeclosure": false,
+          "isPriceReduced": false,
+          "isAddressSuppressed": false,
+          "bed": 4,
+          "bath": "1+",
+          "lotSize": 2614,
+          "photo": null,
+          "photoCount": 0,
+          "price": 214933,
+          "priceDisplay": "$214,933",
+          "propertyType": "single_family",
+          "sqft": 1888,
+          "sqft_display": "1,888",
+          "address": "14 Dow St",
+          "city": "Nashua",
+          "county": "Hillsborough",
+          "state": "New Hampshire",
+          "zip": "03064",
+          "pinPrice": null,
+          "data_source": null,
+          "agent_id": null
+
+  -}
+
+data Item = Item {
+    id              :: Text
+  , bed             :: Maybe Int
+  , bath            :: Maybe Text
+  , lotSize         :: Int
+  , price           :: Int
+  , sqft            :: Int
+  , address         :: Text
+  , city            :: Text
+  , isForeclosure   :: Bool
+  , propertyType    :: Text
+  , isStatusPending :: Maybe Bool
+  }
+  deriving (Show, Generic)
+
+instance FromJSON Item
+
+{-
+
+curl 'https://www.realtor.com/search_result.json' \
+  -H 'content-type: application/json' \
+  -H 'accept: application/json, text/javascript, */*; q=0.01' \
+  --data-binary '{"search_criteria":"Madison_NJ","city":"Madison","state":"NJ","facets":{"days_on_market":null,"show_listings":null,"foreclosure":null},"search_controller":"Search::RecentlySoldController","types":["property"]}' \
+  --compressed | jq .
+
+-}
+
+
+-- https://www.realtor.com/search_result.json
+
+type API =
+    "pagination_result.json"
+  :> Header "content-type" Text
+  :> Header "accept" Text
+  :> ReqBody '[JSON] SearchCriteria :> Post '[JSON] QueryResponse
+
+
+
+api :: Proxy API
+api = Proxy
+
+search
+  :: Maybe Text
+  -> Maybe Text
+  -> SearchCriteria
+  -> ClientM QueryResponse
+search = client api
+
+
+-- | Simple data type to represent the target of HTTP requests
+--   for servant's automatically-generated clients.
+data BaseUrl = BaseUrl
+  { baseUrlScheme :: Scheme -- ^ URI scheme to use
+  , baseUrlHost   :: [Char]   -- ^ host (eg "haskell.org")
+  , baseUrlPort   :: Int      -- ^ port (eg 80)
+  , baseUrlPath   :: [Char]   -- ^ path (eg "/a/b/c")
+  }
+
+
+
